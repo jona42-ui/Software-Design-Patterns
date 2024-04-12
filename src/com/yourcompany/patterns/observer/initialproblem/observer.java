7ffeabe60907ef8d6e9319411ef0c7d6f619a40e
jona42-ui <jonathanthembo123@gmail.com>
2024-04-12 13:51:31 +0300

    here another commit

--- src/com/yourcompany/patterns/observer/initialproblem/observer.java
+++ src/com/yourcompany/patterns/observer/initialproblem/observer.java
@@ -1,3 +1,5 @@
+package com.yourcompany.patterns.observer.initialproblem;
+
 // Simulate changes in EHR
 class EHRSystem {
     public void updateBillingRecords() {
@@ -7,7 +9,7 @@ class EHRSystem {
 }
 
 // Main class manually triggers update in billing system
-public class Main {
+public class observer {
     public static void main(String[] args) {
         // Simulate change in EHR triggering update in billing system
         EHRSystem ehrSystem = new EHRSystem();
